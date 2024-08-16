8637e2e61a2b66976cacc9e9535dac2854eb5e9d
Alex Gorbunov <alexandr.gorbunov@smart-steel-technologies.com>
2024-08-16 09:27:49 +0200

    update theme

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "cyberpunk-neon-theme"
+rootProject.name = "cyberpunk-neon-theme-updated"
